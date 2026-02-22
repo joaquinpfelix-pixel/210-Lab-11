7502a41efa200ce8cc888b4efa7966510d1ade55
JoaquinF <joaquinpfelix@gmail.com>
2026-02-22 15:07:35 -0800

    Check 2

--- 210-Lab-11.cpp
+++ 210-Lab-11.cpp
@@ -1,14 +1,45 @@
+// COMSC-210 | Lab 11 | Joaquin Felix
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-/*
-    Struct: Student
-    Represents a single student.
-*/
+// Struct: Student
+// Represents one student with a dynamic array of courses
+struct Student {
+    string name;
+    int studentID;
+
+    // Pointer to dynamic array of course names
+    string* courses;
+    int courseCount;
+};
+
+
+void addCourse(Student& student, string course);
+
+
 
 int main (){
 
     
+}
+void addCourse(Student& student, string course){
+    // Create a new array with one extra slot
+    string* newCourses = new string[student.courseCount + 1];
+
+    // Copy existing courses into new array
+    for (int i = 0; i < student.courseCount; i++){
+        newCourses[i] = student.courses[i];
+    }
+
+    newCourses[student.courseCount] = course;
+    delete[i] student.courses;
+    student.courses = new newCourses;
+    student.courseCount++;
+}
+
+void printStudent(const Student& student){
+    cout << "Name: " << student.name
 }
